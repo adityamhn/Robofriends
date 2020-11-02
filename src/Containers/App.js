bd96229426a77fc88db9223b6407274c62cdc441
adityamhn <aditya530026@gmail.com>
2020-11-02 20:12:55 +0530

    using react hooks

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,47 +1,52 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from './../Components/CardList';
 import SearchBox from './../Components/SearchBox';
 import Scroll from './../Components/Scroll';
 import ErrorBoundry from './../Components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			robots: [],
-	        searchfield: ' '
-		}
-	}
+function App () {
+	// constructor() {
+	// 	super()
+	// 	this.state = {
+	// 		robots: [],
+	//         searchfield: ' '
+	// 	}
+	// }
+	const [robots,setRobots] = useState([])
+	const [searchfield,setSearchfield] = useState("")
 
-	componentDidMount() {
+	// componentDidMount() {
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 	.then(response =>response.json())
+	// 	.then(users => {this.setState({robots: users})});
+	// }
+
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response =>response.json())
-		.then(users => {this.setState({robots: users})});
-	}
+		.then(users => {setRobots(users)});
+	},[])
     
-    onSearchChange = (event) => {
-    	this.setState({ searchfield: event.target.value})
+const onSearchChange = (event) => {
+    	setSearchfield(event.target.value)
     	}
 
-	render() {
-        const {robots,searchfield} = this.state;
-
 		const filteredRobots = robots.filter(robot => {
-    		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    		return robot.name.toLowerCase().includes(searchfield.toLowerCase()) || robot.email.toLowerCase().includes(searchfield.toLowerCase()) ;
     	});
         return robots.length === 0 ?
         <h1 className="tc">LOADING!!</h1> :
 		<div className="tc">
 		<h1 className="f1">ROBO FRIENDS</h1>
-		<SearchBox onSearchChange={this.onSearchChange}/>
+		<SearchBox onSearchChange={onSearchChange}/>
 		<Scroll>
 		<ErrorBoundry>
         <CardList robots={filteredRobots} />
         </ErrorBoundry>
           </Scroll>
      </div>
-	  }
+	  
 	}
 
 export default App;
